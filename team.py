8380ce6def92640e62fe4b50008302676db0a28a
Ryan Lee <rjlguitar@gmail.com>
2020-11-22 14:39:55 -0800

    added attack method

--- team.py
+++ team.py
@@ -1,3 +1,5 @@
+import random
+
 class Team:
     def __init__(self, name):
         self.name = name
@@ -8,8 +10,8 @@ class Team:
     
     def remove_hero(self, name):
         foundHero = False
-        for i in self.heroes:
-            if i.name == name:
+        for hero in self.heroes:
+            if hero.name == name:
                 self.heroes.remove(hero)
                 foundHero = True
         if not foundHero:
@@ -18,5 +20,27 @@ class Team:
     def view_heroes(self):
         for i in self.heroes:
             print(i)
-
     
+    def stats(self):
+        for hero in self.heroes:
+            kd = hero.kills / hero.deaths
+            print("{} Kill/Deaths:{}".format(hero.name,kd))
+
+    def revive_heroes(self, health=100):
+        for hero in self.heroes:
+            hero.current_hp = health
+
+    def attack(self, other_team):
+        living_heroes = list()
+        living_opponents = list()
+
+        for hero in self.heroes:
+            living_heroes.append(hero)
+
+        for hero in other_team.heroes:
+            living_opponents.append(hero)
+        
+        while len(living_heroes) > 0 and len(living_opponents)> 0:
+            randomhero1 = random.choice(self.living_heroes)
+            randomhero2 = random.choice(other_team.living_opponents)
+            randomhero1.fight(randomhero2)
